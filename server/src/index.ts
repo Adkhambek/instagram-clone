d09cdb45d8b70260bf58af90dc51946d6dd83c44
Adham <muzaffarov.adham@gmail.com>
2021-12-25 12:53:59 +0500

    graphql launched

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,60 @@
 import connectDB from "./utils/database";
 import UserModel from "./models/UserModel";
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+import http from "http";
+import keys from "./config/keys";
 
-(async function () {
-    await connectDB();
-    const newUser = new UserModel({
-        email: "muzaffarov.adham@gmail.com",
-        fullName: "Muzaffarov Adham",
-        username: "Heaven_8",
-        password: "heaven12345678",
+const PORT = keys.port;
+
+const typeDefs = gql`
+    type Query {
+        users: [Users]
+    }
+    type Users {
+        email: String
+        fullName: String
+        username: String
+        password: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return UserModel.find();
+        },
+    },
+};
+
+const startApolloServer = async (typeDefs: any, resolvers: any) => {
+    const app = express();
+    app.use("/public", express.static("./uploads"));
+    const httpServer = http.createServer(app);
+    const apollo = new ApolloServer({
+        resolvers,
+        typeDefs,
     });
-    await newUser.save();
-})();
+    await connectDB();
+    await apollo.start();
+    apollo.applyMiddleware({ app });
+    await new Promise<void>((resolve) =>
+        httpServer.listen({ port: PORT }, resolve)
+    );
+    console.log(
+        `🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}`
+    );
+};
+
+startApolloServer(typeDefs, resolvers);
+
+// (async function () {
+//     await connectDB();
+//     const newUser = new UserModel({
+//         email: "muzaffarov.adham@gmail.com",
+//         fullName: "Muzaffarov Adham",
+//         username: "Heaven_8",
+//         password: "heaven12345678",
+//     });
+//     await newUser.save();
+// })();
